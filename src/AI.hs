fc47901f6429e2287cc8bda601e6caa7dee89d55
Sjur Millidahl <sjur.millidahl@finn.no>
2017-12-04 08:31:41 +0100

    AI spec, pawn advancement

--- src/AI.hs
+++ src/AI.hs
@@ -1,4 +1,4 @@
-module AI (first, evaluate, firstBest) where
+module AI (first, evaluate, firstBest, pawnAdvancement) where
 
 import Chess
 
@@ -18,7 +18,26 @@ lowest :: [(Position, Float)] -> Position
 lowest t = fst $ foldl (\(p1, f1) (p2, f2) -> if f1 < f2 then (p1, f1) else (p2, f2)) (Chess.emptyBoard, 0.0 :: Float) t
 
 evaluate :: Position -> Float
-evaluate p = whitePieces' p - blackPieces' p
+evaluate p = whitePieces' p - blackPieces' p + pawnAdvancement p
+
+pawnAdvancement :: Position -> Float
+pawnAdvancement pos = sum $ fmap pawnPosValue pos
+
+pawnPosValue :: (Square, Maybe Piece) -> Float
+pawnPosValue (('c', r), Just (Pawn White)) = r' r * 0.06
+pawnPosValue (('f', r), Just (Pawn White)) = r' r * 0.06
+pawnPosValue (('d', r), Just (Pawn White)) = r' r * 0.07
+pawnPosValue (('e', r), Just (Pawn White)) = r' r * 0.07
+pawnPosValue ((x, r), Just (Pawn White)) = r' r * 0.05
+pawnPosValue (('c', r), Just (Pawn Black)) = (9 - r' r) * (-0.06)
+pawnPosValue (('f', r), Just (Pawn Black)) = (9 - r' r) * (-0.06)
+pawnPosValue (('d', r), Just (Pawn Black)) = (9 - r' r) * (-0.07)
+pawnPosValue (('e', r), Just (Pawn Black)) = (9 - r' r) * (-0.07)
+pawnPosValue ((x, r), Just (Pawn Black)) =   (9 - r' r) * (-0.05)
+pawnPosValue x = 0.00
+
+r' :: Int -> Float
+r' n = fromIntegral n :: Float
 
 whitePieces' :: Position -> Float
 whitePieces' pos = count pos Chess.whitePieces
---
+++ test/AISpec.hs
@@ -0,0 +1,20 @@
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+
+import Chess
+import Move
+import Printer
+import AI
+
+main :: IO ()
+main = hspec $ do
+    describe "AI" $ do
+        it "evaluates the start position as reasonably balanced" $ do
+            let e = AI.evaluate Chess.startPosition
+            e `shouldSatisfy` (> (-1.1))
+            e `shouldSatisfy` (< 1.1)
+        it "evaluate advanced pawns over home pawns" $ do
+            let p = Chess.makeMoves [Chess.startPosition] [ (('e', 2), ('e', 4)) ]
+            let e = AI.evaluate $ head p
+            e `shouldSatisfy` (> 0.0)
