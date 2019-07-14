40ffb966c315c3ed289c0a466da49c2b2bb8491b
Sjur Millidahl <sjur.millidahl@finn.no>
2019-07-14 21:03:53 +0200

    sub 7 sec bench

--- bench.sh
+++ bench.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ./stackw install --profile --work-dir=.stackprofile --copy-bins && \
-time ./stackw exec --work-dir=.stackprofile -- hchess-profiled +RTS -N4 -p -s -qg -K100M
+time ./stackw exec --work-dir=.stackprofile -- hchess-profiled +RTS -N8 -p -s -qg
