6389be1afbb8e96781a878a47bcbdf6a5c8e5107
Sjur Millidahl <sjur.millidahl@finn.no>
2019-09-16 09:19:57 +0200

    Benchmarks looking good

--- bench.sh
+++ bench.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ./stackw install --profile --work-dir=.stackprofile --copy-bins && \
-time ./stackw exec --work-dir=.stackprofile -- hchess-profiled +RTS -N8 -p -s -qg
+time ./stackw -v exec --work-dir=.stackprofile -- hchess-profiled +RTS -N8 -p -s -qg
