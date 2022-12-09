1b38139113f585d1ddc0444a01dae1f7f9e9d383
Sjur Millidahl <smillida@cisco.com>
2022-12-09 14:21:04 +0100

    IntMap works

--- src/Board.hs
+++ src/Board.hs
@@ -2,31 +2,27 @@ module Board (Board, set, (?!), empty64, searchIdx, diff) where
 
 import Control.DeepSeq (NFData)
 import Data.Foldable (toList)
-import Data.Sequence (Seq, fromList, index, update)
-import qualified Data.Sequence as Seq
+import qualified Data.IntMap.Strict as IMap
 import Data.Word (Word8)
 import GHC.Generics (Generic)
 
-type Board a = Seq a
-
--- deriving stock (Eq, Show, Ord, Generic)
--- deriving anyclass (NFData)
+type Board a = IMap.IntMap a
 
 set :: Board a -> Word8 -> a -> Board a
-set s w a = update (fromIntegral w) a s
+set s w a = IMap.insert (fromIntegral w) a s
 
 (?!) :: Board a -> Word8 -> a
-(?!) s w = s `index` fromIntegral w
+(?!) s w = s IMap.! fromIntegral w
 
 empty64 :: a -> Board a
-empty64 a = fmap (const a) (fromList [0 .. 63])
+empty64 a = IMap.fromList $ [0..63] `zip` repeat a
 
 -- tune this
 searchIdx :: NFData a => Board a -> (Word8 -> Bool) -> (a -> Bool) -> [(Word8, a)]
 searchIdx s idxPred piecePred =
     [0 .. 63 :: Word8]
         >>= ( \i ->
-                let mP = s `index` fromIntegral i
+                let mP = s IMap.! fromIntegral i
                  in [(i, mP) | idxPred i && piecePred mP]
             )
 
