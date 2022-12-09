0438cbf72edece6258d640f2abf4ecea3fbbe53d
Sjur Millidahl <smillida@cisco.com>
2022-12-09 14:12:43 +0100

    board as a seq

--- src/Board.hs
+++ src/Board.hs
@@ -3,33 +3,32 @@ module Board (Board, set, (?!), empty64, searchIdx, diff) where
 import Control.DeepSeq (NFData)
 import Data.Foldable (toList)
 import Data.Sequence (Seq, fromList, index, update)
+import qualified Data.Sequence as Seq
 import Data.Word (Word8)
 import GHC.Generics (Generic)
 
-data Board a = Board (Seq a)
-    deriving stock (Eq, Show, Ord, Generic)
-    deriving anyclass (NFData)
+type Board a = Seq a
 
-instance Functor Board where
-    fmap f (Board x) = Board (f <$> x)
-
-instance Foldable Board where
-    foldr f b (Board a) = foldr f b a
+-- deriving stock (Eq, Show, Ord, Generic)
+-- deriving anyclass (NFData)
 
 set :: Board a -> Word8 -> a -> Board a
-set (Board s) w a = Board (update (fromIntegral w) a s)
+set s w a = update (fromIntegral w) a s
 
 (?!) :: Board a -> Word8 -> a
-(?!) (Board s) w = s `index` fromIntegral w
+(?!) s w = s `index` fromIntegral w
 
 empty64 :: a -> Board a
-empty64 a = Board $ fmap (const a) (fromList [0 .. 63])
+empty64 a = fmap (const a) (fromList [0 .. 63])
 
 -- tune this
 searchIdx :: NFData a => Board a -> (Word8 -> Bool) -> (a -> Bool) -> [(Word8, a)]
-searchIdx (Board s) idxPred piecePred =
-    let indexed = [0 .. 63] `zip` toList s
-     in filter (\(square, mP) -> idxPred square && piecePred mP) indexed
+searchIdx s idxPred piecePred =
+    [0 .. 63 :: Word8]
+        >>= ( \i ->
+                let mP = s `index` fromIntegral i
+                 in [(i, mP) | idxPred i && piecePred mP]
+            )
 
 diff :: (NFData a, Eq a) => Board a -> Board a -> [(Word8, a)]
 diff a b = searchIdx b (\w8 -> a ?! w8 /= b ?! w8) (const True)
