4b7d4b09528a4583942acfed25d713b783fd8905
Sjur Millidahl <smillida@cisco.com>
2023-02-20 17:10:29 +0100

    flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,14 @@
 {
   description = "hChess";
 
+  inputs = {
+      nixpkgs = ./nixpkgs.nix;
+      #  sha256 = "sha256:02k1lpjg7yz1h87i6wjv332djz06z8kqcg8mmkxp923kvx52d23m";
+      
+  };
+
   outputs = { self, nixpkgs }: {
 
     packages.x86_64-darwin.hchess = import ./release.nix;
-
-    packages.x86_64-darwin.default = self.packages.x86_64-darwin.hchess;
-
   };
 }
