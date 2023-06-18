aaf225ea497b5bce70be129908a4734ea7032e39
Sjur Millidahl <smillida@cisco.com>
2023-06-18 21:29:30 +0200

    tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,1 +1,15 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+module Main where
+
+import qualified AISpec
+import qualified CacheSpec
+import qualified MoveSpec
+import qualified PGNSpec
+import Test.Hspec
+import Relude
+
+main :: IO ()
+main = hspec $ do
+    AISpec.spec
+    CacheSpec.spec
+    MoveSpec.spec
+    PGNSpec.spec
