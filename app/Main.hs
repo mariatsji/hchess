e599c77f0285bfeb944cbec55344cd2b82d4e558
Sjur Millidahl <sjur.millidahl@statnett.no>
2026-01-07 10:58:14 +0100

    prepare for temporary

--- app/Main.hs
+++ app/Main.hs
@@ -30,7 +30,7 @@ main = do
 
 mkContext :: [String] -> IO AppContext
 mkContext params = do
-  pos <- findPGN params
+  pos <- startFromPos params
   style <- findStyle params
   pure
     AppContext
@@ -59,8 +59,8 @@ findBlack params
 findStyle :: [String] -> IO Style
 findStyle params = pure if "bright" `elem` params then brightTheme else darkTheme
 
-findPGN :: [String] -> IO (Maybe Position)
-findPGN params = do
+startFromPos :: [String] -> IO (Maybe Position)
+startFromPos params = do
   let (_, found) = partitionEithers $ parseOnly pgnLocation . pack <$> params
   case found of
     [] -> pure Nothing
