d41eaa6d0a83715c70e1fce4863f1733cec18a2f
Sjur Millidahl <sjurmillidahl@gmail.com>
2026-04-25 14:25:19 +0200

    chore: remove unused

--- src/Evaluation.hs
+++ src/Evaluation.hs
@@ -9,21 +9,14 @@ module Evaluation
   )
 where
 
-import Board (searchIdx)
 import Chess
   ( Status (..),
     determineStatus,
     positionTree,
     threefoldrepetition,
-    toSquaresBishop',
-    toSquaresKnight',
-    toSquaresQueen',
-    toSquaresRook',
     (<-$->),
   )
-import Control.Parallel (par)
 import Data.List (maximum, minimum)
-import GHC.Conc (pseq)
 import Position
 import Relude
 
@@ -89,22 +82,6 @@ evaluate' pos =
         then Evaluated pos 0.0 Remis
         else Evaluated pos (evaluate (m pos)) playOn
 
--- ideas:
--- trade when leading
-_evaluate :: Snapshot -> Float
-_evaluate snp =
-  foldr
-    ( \(w, mP) acc ->
-        case mP of
-          Nothing -> acc
-          Just pie ->
-            let pieceVal = force $ valueOf pie
-                impactVal = force $ impactArea snp pie (unHash w)
-             in pieceVal `par` impactVal `pseq` acc + pieceVal + impactVal
-    )
-    (bishopPair snp)
-    (toList' snp)
-
 -- much faster evaluate function
 evaluate :: Snapshot -> Float
 evaluate snp =
@@ -123,26 +100,3 @@ valueOf (King c) = colorFactor c * 100.0
 
 colorFactor :: Color -> Float
 colorFactor c = if c == Black then (-1) else 1
-
-impactArea :: Snapshot -> Piece -> Square -> Float
-impactArea _ (Pawn White) (Square _ r) = 0.005 * fromIntegral r
-impactArea _ (Pawn Black) (Square _ r) = (-0.005) * fromIntegral (9 - r)
-impactArea _ (King _) _ = 0
-impactArea snp (Knight c) s =
-  let reachables = fromIntegral . length $ toSquaresKnight' snp c s
-   in colorFactor c * reachables * 0.01
-impactArea snp (Bishop c) s =
-  let reachables = fromIntegral . length $ toSquaresBishop' snp c s
-   in colorFactor c * reachables * 0.01
-impactArea snp (Rook c) s =
-  let reachables = fromIntegral . length $ toSquaresRook' snp c s
-   in colorFactor c * reachables * 0.01
-impactArea snp (Queen c) s =
-  let reachables = fromIntegral . length $ toSquaresQueen' snp c s
-   in colorFactor c * reachables * 0.01
-
-bishopPair :: Snapshot -> Float
-bishopPair snp =
-  let whiteBishops = searchIdx snp (const True) (== Just (Bishop White))
-      blackBishops = searchIdx snp (const True) (== Just (Bishop Black))
-   in (if length whiteBishops == 2 then 0.25 else 0.00) - (if length blackBishops == 2 then 0.25 else 0.00)
